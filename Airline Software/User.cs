ac5ab510605584a34e3f15f3ad556a39b62b6913
Reece Pohlman <reecepohlman@gmail.com>
2023-03-30 16:52:16 -0400

    added base user class

---
+++ Airline Software/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline_Software
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Id { get; }
+
+        public void ChangePassword(string newPassword)
+        {
+            Password = newPassword;
+        }
+    }
+}
